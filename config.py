e35b0771ccf2c3d3938c7a8c0857062e93e1d1a0
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-07-06 23:22:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Your Bot token in the quotes taken from @BotFather on Telegram
+BOT_TOKEN = "5303637649:AAEZvD4CtFO7JcGaoz1D75pVsNtNtgQKjUY"
+
+# Project Name of your Project
+PROJECT_NAME = "GitGram"
+
+# If you forked the Repository, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/STD-DEEPANSHU/GitGram"
